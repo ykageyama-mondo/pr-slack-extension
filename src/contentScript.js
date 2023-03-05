76ff1b4864f7aa63b0510b0fe0291a12c79301e8
Yuichi <yuichi.kageyama@mondo.com.au>
2023-03-06 09:30:36 +1100

    fix: remount when node is orphaned

--- src/contentScript.js
+++ src/contentScript.js
@@ -20,7 +20,7 @@ function getContent() {
 
   const url = window.location.href
   const title = document.body.querySelector('bdi.markdown-title').innerHTML
-  const comment = document.body.querySelector(`div#${descId} div.comment-body p`).innerText
+  const comment = document.body.querySelector(`div#${descId} div.comment-body`).innerText
   return {
     url,
     title,
@@ -28,14 +28,31 @@ function getContent() {
   }
 }
 
-const mount = () => {
+const getDiscussionHeader = () => document.body.querySelector('div#partial-discussion-header')
+
+let copyBtn
+
+const createCopyBtn = () => {
   const cont = document.body.querySelector('div.gh-header-actions')
-  const copyBtn = document.createElement('button')
+  copyBtn = document.createElement('button')
   copyBtn.className = 'btn btn-sm d-inline-block float-left float-none m-0 mr-md-1 flex-md-order-2'
   copyBtn.innerText = 'Copy'
   copyBtn.addEventListener('click', async () => {
     await chrome.runtime.sendMessage({message: 'copy-click'})
   })
   cont.prepend(copyBtn)
+}
+
+const mount = () => {
+  const header = getDiscussionHeader()
+  const observer = new MutationObserver((ele) => {
+    const hT = ele.find(v => v.target.isSameNode(getDiscussionHeader()))
+    if (hT) {
+      if (copyBtn)
+        copyBtn.remove()
+      createCopyBtn()
+    }
+  })
+  observer.observe(header.parentNode, {childList: true, subtree: true, attributes: true})
 }
 mount()
